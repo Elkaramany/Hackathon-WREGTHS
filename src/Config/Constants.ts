c13bc7bcce699f14f887393cc53df2f7036d869e
wysohn <wysohn2002@naver.com>
2022-11-12 12:35:13 -0800

    follow global style

--- src/Config/Constants.ts
+++ src/Config/Constants.ts
@@ -1,43 +1,49 @@
-import { scale, verticalScale } from 'react-native-size-matters';
-import { Platform, StyleSheet, Dimensions } from 'react-native';
+import {scale, verticalScale} from 'react-native-size-matters';
+import {Platform, StyleSheet, Dimensions} from 'react-native';
 
 const IOS: boolean = Platform.OS === 'ios';
 const ANDROID: boolean = Platform.OS === 'android';
-const WIDTH: number = Dimensions.get('window').width
-const HEIGHT: number = Dimensions.get('window').height
+const WIDTH: number = Dimensions.get('window').width;
+const HEIGHT: number = Dimensions.get('window').height;
 
 const Colors = {
     backGround: '#FFF',
-    primary: "#204ECF",
-    secondary: "#03CC83",
-    tertiary: "#262D3D",
-}
+    primary: '#204ECF',
+    secondary: '#03CC83',
+    tertiary: '#262D3D',
+};
 
 const GlobalStyles = StyleSheet.create({
     centeredContainer: {
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     headerTextStyle: {
         fontSize: verticalScale(5),
-        fontWeight: "400",
-        color: Colors.secondary
+        fontWeight: '400',
+        color: Colors.secondary,
     },
     rowBetween: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     rowAround: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    columnAround: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
     },
     rowCenter: {
         flexDirection: 'row',
         justifyContent: 'center',
-        alignItems: 'center'
-    },rowWrap:{
+        alignItems: 'center',
+    },
+    rowWrap: {
         display: 'flex',
         flexDirection: 'row',
         flexWrap: 'wrap',
@@ -46,32 +52,39 @@ const GlobalStyles = StyleSheet.create({
         marginBottom: verticalScale(30),
         width: scale(90),
         borderRadius: scale(50),
-    }, buttonContainer: {
+    },
+    buttonContainer: {
         backgroundColor: Colors.primary,
         justifyContent: 'center',
         alignItems: 'center',
         padding: scale(2.4),
         borderRadius: scale(3),
         margin: verticalScale(2),
-    }, buttonText: {
+    },
+    buttonText: {
         fontSize: verticalScale(3),
         fontWeight: 'bold',
-        color: Colors.primary
-    }, headerContainer: {
-        height: verticalScale(20)
-    }, regularText: {
+        color: Colors.primary,
+    },
+    headerContainer: {
+        height: verticalScale(20),
+    },
+    regularText: {
         fontSize: scale(20),
-        color: Colors.tertiary
-    }, bottomContainer: {
+        color: Colors.tertiary,
+    },
+    bottomContainer: {
         flex: 1,
         justifyContent: 'flex-end',
         marginBottom: verticalScale(15),
-    }, bottomAbsoluted: {
+    },
+    bottomAbsoluted: {
         position: 'absolute',
         bottom: 0,
         marginBottom: verticalScale(15),
-    }, shadow: {
-        shadowColor: "#000",
+    },
+    shadow: {
+        shadowColor: '#000',
         shadowOffset: {
             width: 0,
             height: 2,
@@ -80,14 +93,17 @@ const GlobalStyles = StyleSheet.create({
         shadowRadius: 3.84,
 
         elevation: 5,
-    }
-})
+    },
+});
 
 const textInputTheme = {
     colors: {
-        placeholder: Colors.primary, text: Colors.primary, primary: Colors.primary,
-        underlineColor: Colors.primary, background: Colors.primary
-    }
-}
+        placeholder: Colors.primary,
+        text: Colors.primary,
+        primary: Colors.primary,
+        underlineColor: Colors.primary,
+        background: Colors.primary,
+    },
+};
 
-export { Colors, textInputTheme, GlobalStyles, IOS, ANDROID, WIDTH, HEIGHT };
+export {Colors, textInputTheme, GlobalStyles, IOS, ANDROID, WIDTH, HEIGHT};
